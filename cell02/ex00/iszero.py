693c2c31c7482c0f99987a9fa37ac1e2be1cb456
67051165-Sasirada-Charoensri <67051165@kmitl.ac.th>
2025-09-16 20:10:53 +0700

    executable

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -1,7 +1,6 @@
-i = input() 
-if (i < '0' || i > '9')
-    return (null)
-num = (int(i))
+#!/usr/bin/env python3
+
+num = int(input())
 if (num == 0) :
     print("This number is equal to zero.")
 else :
