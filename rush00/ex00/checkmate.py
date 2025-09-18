6c65c5de13835e8e0d6096396754dbf3f4f783db
67051165-Sasirada-Charoensri <67051165@kmitl.ac.th>
2025-09-18 23:31:12 +0700

    rush

--- rush00/ex00/checkmate.py
+++ rush00/ex00/checkmate.py
@@ -1,27 +1,57 @@
 #!/usr/bin/env python3
 
-def issquare(rows, n) :
+def is_square(rows, n) -> bool:
     if not all(len(r) == n for r in rows) :
         print("ERROR! BOARD IS NOT SQUARE.")
         return False
     return True
 
-def isempty(rows, n) :
+def is_empty(cell) -> bool :
     pieces = ['P', 'B', 'R', 'Q', 'K']
+    if cell not in pieces
 
+def check_kings(kings) -> bool :
+    if (len(kings) != 1) :
+        print("ERROR! MORE THAN 1 KING.")
+        return False
+    return True
 
-def checkkings(kings) :
-    if (len(kings) !=)
+def check_pawn(dir, rows, cur_x, cur_y, k_pos) -> bool :
+    for x, y in dir :
+        if (cur_x + x, cur_y) == k_pos :
+            return True
+    return False
 
 def checkmate(board: str) :
     rows = board.strip().split('\n')
     print(rows)
     n = len(rows)
 
-    if not (issquare(rows, n)) : 
+    if not (is_square(rows, n)) : 
+        return
+
+    if not (is_empty(rows, n)) :
         return
 
     pieces = ['P', 'B', 'R', 'Q', 'K']
     kings = [(x, y) for x in range(n) for y in range(n) if rows[x][y] == 'K']
-    print(kings)
+    print("Kings found:", kings)
+    if not check_kings(kings) :
+        return
+    king_position = kings[0]
+    print("Kings position:", kings)
 
+    pawn_dir = [(-1, -1), (-1, 1)]
+    bishop_dir = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+    rook_dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    queen_dir = bishop_dir + rook_dir
+
+    for x in range(n) :
+        for y in range(n) :
+            piece = rows[x][y]
+            if piece not in pieces :
+                continue
+            if (piece == 'P') :
+                if (check_piece(pawn_dir, rows)) :
+                    print("Success")
+                    return
