702cdb890fdcf6493cb0d2f6b667a23e4b02967c
67051165-Sasirada-Charoensri <67051165@kmitl.ac.th>
2025-09-18 13:25:32 +0700

    cell05/ex12_edited

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -8,7 +8,10 @@ if (params_len == 2) :
     for i in range(len(sys.argv[1])) :
         if (sys.argv[1][i] == 'z') :
             count += 1
-    for i in range(0, count) :
-        print("z", end="" if (i < count - 1) else "\n")
+    if (count > 0) :
+        for i in range(0, count) :
+            print("z", end="" if (i < count - 1) else "\n")
+    else :
+        print("none")
 else :
     print("none")
